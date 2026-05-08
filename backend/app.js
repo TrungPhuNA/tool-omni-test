abf40f5f5a99cbeb251e3695032a7ec24d992550
phuphan <codethue94@gmail.com>
2026-05-08 18:02:13 +0700

    chore: increase payload size limits for express json and urlencoded parsers

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,8 @@ app.set('io', io);
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Socket.io connection
 io.on('connection', (socket) => {
